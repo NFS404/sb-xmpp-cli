a71604f889e6a1910f7cbb65ec498ef82796773c
Nilzao <288571+nilzao@users.noreply.github.com>
2020-09-17 23:52:04 -0400

    powerup implementation

--- src/main/java/io/github/sbcloudrace/sbxmppcli/SbXmppCliApplication.java
+++ src/main/java/io/github/sbcloudrace/sbxmppcli/SbXmppCliApplication.java
@@ -4,9 +4,11 @@ import io.github.sbcloudrace.sbxmppcli.cli.SbXmppClient;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SbXmppCliApplication {
 
     public static void main(String[] args) {
