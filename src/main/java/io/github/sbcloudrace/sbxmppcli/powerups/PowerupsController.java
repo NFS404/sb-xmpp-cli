a55f051524e1b7eaa4bd1b698979422347be9ede
Nilzao <288571+nilzao@users.noreply.github.com>
2020-09-19 23:55:13 -0400

    moving microservices

--- src/main/java/io/github/sbcloudrace/sbxmppcli/powerups/PowerupsController.java
+++ src/main/java/io/github/sbcloudrace/sbxmppcli/powerups/PowerupsController.java
@@ -3,7 +3,6 @@ package io.github.sbcloudrace.sbxmppcli.powerups;
 import io.github.sbcloudrace.sbxmppcli.cli.SbXmppClient;
 import io.github.sbcloudrace.sbxmppcli.cli.jaxb.xmpp.PowerupActivated;
 import io.github.sbcloudrace.sbxmppcli.cli.jaxb.xmpp.XMPP_ResponseType;
-import io.github.sbcloudrace.sbxmppcli.session.SbSessionServiceProxy;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +13,10 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class PowerupsController {
 
-    private final SbSessionServiceProxy sbSessionServiceProxy;
-
-    @RequestMapping(value = "/activated/{securityToken}/{targetId}/{powerupHash}/{receivers}",
+    @RequestMapping(value = "/activated/{personaId}/{targetId}/{powerupHash}/{receivers}",
             method = RequestMethod.PUT)
     @ResponseBody
-    public void activated(@PathVariable String securityToken,
+    public void activated(@PathVariable Long personaId,
                           @PathVariable("targetId") Long targetId,
                           @PathVariable Integer powerupHash,
                           @PathVariable("receivers") String receivers) {
@@ -27,7 +24,7 @@ public class PowerupsController {
         PowerupActivated powerupActivated = new PowerupActivated();
         powerupActivated.setId(powerupHash);
         powerupActivated.setTargetPersonaId(targetId);
-        powerupActivated.setPersonaId(sbSessionServiceProxy.activePersonaId(securityToken));
+        powerupActivated.setPersonaId(personaId);
         powerupActivatedResponse.setPowerupActivated(powerupActivated);
         for (String receiver : receivers.split("-")) {
             long receiverPersonaId = Long.parseLong(receiver);
